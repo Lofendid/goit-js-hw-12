1a3a8044c9e0365c6e5494ee681c7d8d45551d7d
Lofendid <102857014+Lofendid@users.noreply.github.com>
2024-01-15 16:15:22 +0100

    almost_ready

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+import axios from "axios";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 const iconLeft = new URL('../img/icons/arrow-icons.svg#icon-left', import.meta.url).href; 
 const iconRight = new URL('../img/icons/arrow-icons.svg#icon-right', import.meta.url).href; 
-
 const iconError = new URL('/img/icons/error-icon.svg', import.meta.url).href;
+
+const LS_QUERY = 'user-query'
 
 const gallery = document.querySelector('.gallery');
 const galleryForm = document.querySelector('.search-photos');
 const loader = document.querySelector('.loader-wrapper');
+const moreBtn = document.querySelector('.more-btn');
+
+let page = 1;
+let limit = 9;
 
 const mistake = (message) => {
     iziToast.show({
@@ -23,33 +30,34 @@ const mistake = (message) => {
     });
 };
 
-const getPhotos = (userQuery) => {
-    const searchParams = new URLSearchParams({
-        key: '41527103-e0775164a767d3baf6162359a',
-        q: encodeURIComponent(userQuery).replace(/%20/g, '+'),
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-    });
+const getTotalPages = (images) => {
+    return Math.ceil(images.totalHits / limit);
+}
 
-    const BASE_URL = `https://pixabay.com/api/?${searchParams}`;
+const getPhotos = async (userQuery) => {
 
-    fetch(BASE_URL, {
+    const response = await axios.get(`https://pixabay.com/api/`, {
+        params: {
+            key: '41527103-e0775164a767d3baf6162359a',
+            q: encodeURIComponent(userQuery).replace(/%20/g, '+'),
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: limit,
+            page: page,
+        }
     })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Sorry, there are some internal mistakes. Please, try reload the page!');
-            }
-            else return res.json();
-        })
-        .then(res => {
-            const allImages = res.hits;
-            if (res.total === 0) {
-                throw new Error('Sorry, there are no images matching your search query. Please, try again!');
-            }
-            else {
-                const galleryMarkup = allImages.map(({ webformatURL: preview, largeImageURL: original, tags: description }) => {
-                    return `
+    return response.data;
+};
+    const renderImages = (images) => {
+        const allImages = images.hits;
+        if (images.totalHits === 0) {
+            loader.classList.add('visually-hidden');
+            return mistake('Sorry, there are no images matching your search query. Please, try again!');
+        }
+        else {
+            const galleryMarkup = allImages.map(({ webformatURL: preview, largeImageURL: original, tags: description }) => {
+                return `
                         <li class="gallery-item">
                             <a class="gallery-link" href="${original}">
                                 <img
@@ -61,34 +69,55 @@ const getPhotos = (userQuery) => {
                             </a>
                         </li>
                         `
-                }).join('');
-                gallery.innerHTML = galleryMarkup;
-                const lightbox = new SimpleLightbox('.gallery a', {
-                    navText: [
-                        `<svg width="24" height="24"><use href="${iconLeft}"></svg>`,
-                        `<svg width="24" height="24"><use href="${iconRight}"></svg>`
-                    ],
-                    captionsData: "alt",
-                    captionDelay: false,
-                });
-                lightbox.refresh();
-            };
-        })
-        .catch(err => {
-            mistake(err.message);
-        })
-        .finally(() => {
+            }).join('');
+            gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+            const lightbox = new SimpleLightbox('.gallery a', {
+                navText: [
+                    `<svg width="24" height="24"><use href="${iconLeft}"></svg>`,
+                    `<svg width="24" height="24"><use href="${iconRight}"></svg>`
+                ],
+                captionsData: "alt",
+                captionDelay: false,
+            });
+            lightbox.refresh();
             loader.classList.add('visually-hidden');
-        });
-};
+        };
+    };
 
-const handleSubmit = (e) => {
+const onClick = async () => {
+    try {
+        page += 1;
+        const images = await getPhotos(sessionStorage.getItem(LS_QUERY));
+        const totalPages = getTotalPages(images);
+        if (page === totalPages) {
+            moreBtn.classList.add('visually-hidden');
+            mistake('The end of collection has been reached')
+        };
+        renderImages(images);
+    }
+    catch (error) {
+        mistake(error.message);
+    }
+ };
+
+const handleSubmit = async (e) => {
     e.preventDefault();
     gallery.innerHTML = '';
     loader.classList.remove('visually-hidden');
     const userQuery = e.target.elements[0].value.trim().toLowerCase().toString();
-    getPhotos(userQuery);
+    sessionStorage.setItem(LS_QUERY, userQuery);
+    page = 1;
+    const images = await getPhotos(userQuery);
+    const totalPages = getTotalPages(images);
+    renderImages(images);
+    if (page === totalPages) {
+        moreBtn.classList.add('visually-hidden');
+    }
+    else if (page < totalPages) {
+        moreBtn.classList.remove('visually-hidden');
+    };
     e.target.reset();
  };
 
 galleryForm.addEventListener('submit', handleSubmit);
+moreBtn.addEventListener('click', onClick);
